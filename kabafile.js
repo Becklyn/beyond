4eacbace61d38a07b5dc53d954274c3cde545ccc
Jannik Zschiesche <jz@becklyn.com>
2016-10-12 17:38:24 +0200

    Added development configuration

---
+++ kabafile.js
@@ -0,0 +1,16 @@
+const kaba = require("kaba");
+
+const js = kaba.shelf.js({
+    input: "src/js/",
+    output: "../../dist/js"
+});
+const scss = kaba.shelf.scss({
+    input: "src/scss/",
+    output: "../../dist/css"
+});
+
+kaba.task("scss", scss);
+kaba.task("js", js);
+
+kaba.task("", kaba.parallel(scss, js));
+kaba.task("release", kaba.task(""));
