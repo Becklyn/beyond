fe2c22a510890e4ec46ad0d16b60d62be6319a4f
Mario Schmidt <ms@becklyn.com>
2016-11-25 14:32:23 +0100

    refactor tooltip functionality

--- src/js/beyond/tooltip.js
+++ src/js/beyond/tooltip.js
@@ -30,6 +30,17 @@ class Tooltip
 
         /**
          * @private
+         * @type {{top: string, right: string, bottom: string, left: string}}
+         */
+        this.alternativePlacementMap = {
+            top: "bottom",
+            right: "left",
+            bottom: "top",
+            left: "right",
+        };
+
+        /**
+         * @private
          * @type {string}
          */
         this.text = "";
@@ -43,10 +54,10 @@ class Tooltip
         this.placement = this.$emitter.data("placement");
         this.text = this.$emitter.data("text");
 
-        this.registerEventListener();
         this.generateTooltip();
         this.appendTooltip();
-        this.calculateOffset();
+        this.registerEventListener();
+        this.checkForSufficientSpace();
     }
 
     /**
@@ -61,7 +72,7 @@ class Tooltip
     }
 
     /**
-     *
+     * show/hide tooltip
      */
     toggleTooltip ()
     {
@@ -69,7 +80,7 @@ class Tooltip
     }
 
     /**
-     * generates tooltip
+     * generate tooltip structure
      */
     generateTooltip ()
     {
@@ -86,7 +97,7 @@ class Tooltip
     }
 
     /**
-     *
+     * check for .hiddenElementContainer and append $tooltip
      */
     appendTooltip ()
     {
@@ -105,55 +116,110 @@ class Tooltip
     }
 
     /**
-     *
+     * check if sufficient space is available for $tooltip to render
      */
-    calculateOffset ()
+    checkForSufficientSpace ()
+    {
+        const $window = $(window);
+        const $document = $(document);
+        const space = {
+            vertical: this.$tooltip.outerHeight(),
+            horizontal: this.$tooltip.outerWidth(),
+        };
+
+        let placement = this.placement;
+
+        switch (placement)
+        {
+            case "top":
+                space.vertical = this.$emitter.offset().top - $window.scrollTop();
+                break;
+
+            case "bottom":
+                space.vertical = ($document.scrollTop() + $window.innerHeight()) - (this.$emitter.offset().top + this.$emitter.outerHeight());
+                break;
+
+            case "right":
+                space.horizontal = document.body.clientWidth - (this.$emitter.offset().left + this.$emitter.outerWidth());
+                break;
+
+            case "left":
+                space.horizontal = this.$emitter.offset().left;
+                break;
+        }
+
+        placement = this.handlePlacementSwitch(placement, space);
+
+        this.calculateOffset(placement);
+    }
+
+    /**
+     * handle the alternative placement of $tooltip if not sufficient Space is available
+     *
+     * @param {string} placement
+     * @param {{vertical: number, horizontal: number}} space
+     * @returns {string}
+     */
+    handlePlacementSwitch (placement, space)
+    {
+        const alternativePlacement = this.alternativePlacementMap[placement];
+
+        this.$tooltip.removeClass(`${placement} ${alternativePlacement}`);
+        this.$tooltip.addClass(placement);
+
+        if (space.horizontal < this.$tooltip.outerWidth() || space.vertical < this.$tooltip.outerHeight())
+        {
+            this.$tooltip.toggleClass(`${placement} ${alternativePlacement}`);
+            return alternativePlacement;
+        }
+
+        return placement;
+    }
+
+    /**
+     * calculate the position of $tooltip
+     *
+     * @param {string} placement
+     */
+    calculateOffset (placement)
     {
         const centerHorizontal = this.$emitter.offset().left + (this.$emitter.outerWidth() / 2);
         const centerVertical = this.$emitter.offset().top + (this.$emitter.outerHeight() / 2);
 
-        let top = 0;
-        let left = 0;
+        let offsetTop = 0;
+        let offsetLeft = 0;
 
-        switch (this.placement)
+        switch (placement)
         {
-            case ("top"):
-                top = this.$emitter.offset().top;
-                left = centerHorizontal;
+            case "top":
+                offsetTop = this.$emitter.offset().top;
+                offsetLeft = centerHorizontal;
                 break;
-            case ("right"):
-                top = centerVertical;
-                left = this.$emitter.offset().left + (this.$emitter.outerWidth());
+
+            case "bottom":
+                offsetTop = this.$emitter.offset().top + (this.$emitter.outerHeight());
+                offsetLeft = centerHorizontal;
                 break;
-            case ("bottom"):
-                top = this.$emitter.offset().top + (this.$emitter.outerHeight());
-                left = centerHorizontal;
+
+            case "right":
+                offsetTop = centerVertical;
+                offsetLeft = this.$emitter.offset().left + (this.$emitter.outerWidth());
                 break;
-            case ("left"):
-                top = centerVertical;
-                left = this.$emitter.offset().left;
+
+            case "left":
+                offsetTop = centerVertical;
+                offsetLeft = this.$emitter.offset().left;
                 break;
+
             default:
                 break;
         }
 
         this.$tooltip.css({
             position: "absolute",
-            top,
-            left,
+            top: offsetTop,
+            left: offsetLeft,
         });
-    }
-
-    /**
-     *
-     */
-    checkForSufficientSpace ()
-    {
-        // Todo: Remove console.log()
-        // ---------- Log ----------
-        // window.console.log(document.body.clientWidth - (this.$emitter.offset().left + this.$emitter.outerWidth()));
-        // -------------------------
-
     }
 }
 
